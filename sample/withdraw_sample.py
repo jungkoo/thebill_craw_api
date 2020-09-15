98ef9d096486d217ccefda25da8973d86e271d74
mc.jeong <mc.jeong@navercorp.com>
2020-09-15 17:46:05 +0900

    리소스 해제하는 형태로 예시 변경 (windows close)

--- sample/withdraw_sample.py
+++ sample/withdraw_sample.py
@@ -6,13 +6,12 @@ login = Login(user_id='<아이디>', password='<비밀번호>', driver_path="<ch
 login.headless(False)
 
 if __name__ == "__main__":
-    withdraw = WithDraw()
-    for r in withdraw.result():
-        print(r)
-
-    print("# 검색")
-    withdraw.set_start_date("2020-08-14")
-    withdraw.set_member_name("홍길동")
-    withdraw.submit()
-    for r in withdraw.result():
-        print(r)
+    with WithDraw() as withdraw:
+        for r in withdraw.result():
+            print(r)
+        print("# 검색")
+        withdraw.set_start_date("2020-08-14")
+        withdraw.set_member_name("홍길동")
+        withdraw.submit()
+        for r in withdraw.result():
+            print(r)
